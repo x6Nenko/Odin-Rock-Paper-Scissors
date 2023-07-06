7530f3cc1a0567962073ed87e1e393d8a4b3204e
Max I <clickkava@gmail.com>
2023-07-06 20:36:38 +0300

    Add getComputerChoice and playRound functions
    getComputerChoice randomly chooses between rock, papper, and scissors.
    playRound require player and computer choices. Then it determines the winner.

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("yo")
+function getComputerChoice() {
+    const choicesArray = ["Rock", "Paper", "Scissors"]
+    const makeChoice = Math.floor(Math.random() * choicesArray.length);
+
+    return choicesArray[makeChoice]
+};
+
+function playRound(playerSelection, computerSelection) {
+    const player = playerSelection.toLowerCase();
+    const computer = computerSelection.toLowerCase();
+
+    if (player === "rock") {
+        if (computer === "rock") {
+            return console.log("Draw!");
+        } else if (computer === "scissors") {
+            return console.log("You Won! Rock beats Scissors");
+        } else if (computer === "paper") {
+            return console.log("You Lose! Paper beats Rock");
+        }
+    }
+
+    if (player === "paper") {
+        if (computer === "paper") {
+            return console.log("Draw!");
+        } else if (computer === "rock") {
+            return console.log("You Won! Paper beats Rock");
+        } else if (computer === "scissors") {
+            return console.log("You Lose! Scissors beats Paper");
+        }
+    }
+
+    if (player === "scissors") {
+        if (computer === "scissors") {
+            return console.log("Draw!");
+        } else if (computer === "paper") {
+            return console.log("You Won! Scissors beats Paper");
+        } else if (computer === "rock") {
+            return console.log("You Lose! Rock beats Scissors");
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
