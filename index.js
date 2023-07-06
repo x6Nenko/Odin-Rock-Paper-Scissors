dd4a5ecc478f6663333ea06f1049c4f9e0cb4436
Max I <clickkava@gmail.com>
2023-07-06 21:16:36 +0300

    Improve playRound function and add game function
    Made playRound to count the score
    
    The game function uses previously added functions and asks the player to
    make a choice. It plays 5 rounds and determines the winner.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function getComputerChoice() {
-    const choicesArray = ["Rock", "Paper", "Scissors"]
+    const choicesArray = ["Rock", "Paper", "Scissors"];
     const makeChoice = Math.floor(Math.random() * choicesArray.length);
 
-    return choicesArray[makeChoice]
+    return choicesArray[makeChoice];
 };
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playRound(playerSelection, computerSelection) {
     const player = playerSelection.toLowerCase();
@@ -13,33 +16,57 @@ function playRound(playerSelection, computerSelection) {
         if (computer === "rock") {
             return console.log("Draw!");
         } else if (computer === "scissors") {
+            playerScore += 1;
             return console.log("You Won! Rock beats Scissors");
         } else if (computer === "paper") {
+            computerScore += 1;
             return console.log("You Lose! Paper beats Rock");
         }
-    }
+    };
 
     if (player === "paper") {
         if (computer === "paper") {
             return console.log("Draw!");
         } else if (computer === "rock") {
+            playerScore += 1;
             return console.log("You Won! Paper beats Rock");
         } else if (computer === "scissors") {
+            computerScore += 1;
             return console.log("You Lose! Scissors beats Paper");
         }
-    }
+    };
 
     if (player === "scissors") {
         if (computer === "scissors") {
             return console.log("Draw!");
         } else if (computer === "paper") {
+            playerScore += 1;
             return console.log("You Won! Scissors beats Paper");
         } else if (computer === "rock") {
+            computerScore += 1;
             return console.log("You Lose! Rock beats Scissors");
         }
-    }
-}
+    };
+};
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+    for (let index = 0; index < 5; index++) {
+        const computerSelection = getComputerChoice().toLowerCase();
+        const playerSelection = prompt("Whats your choice?").toLowerCase();
+        playRound(playerSelection, computerSelection);
+        console.log(playerScore, computerScore);
+    };
+
+    if (playerScore === computerScore) {
+        console.log("Close one. It's a Draw!");
+    } else if (playerScore > computerScore) {
+        console.log("Player has Won!");
+    } else if (computerScore > playerScore) {
+        console.log("Computer has Won!");
+    };
+    
+    playerScore = 0;
+    computerScore = 0;
+};
+
+game();
